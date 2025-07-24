33011fcd8c71c966ae92d8e50a1650299d3765e8
nilkr47 <145826752+nilkr47@users.noreply.github.com>
2025-07-24 19:42:56 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,13 @@
+import math
+
+num = int(input("Enter a number: "))
+
+
+sqrt_val = math.sqrt(num)
+log_val = math.log(num)
+sine_val = math.sin(num)
+
+print(f"\nResults for the number {num}:")
+print(f" Square Root : {sqrt_val}")
+print(f" logarithm : {log_val}")
+print(f" Sine : {sine_val}")
